1d7e89677cdf77f54a20018fbbe0efa4dd6f6a73
c0demag <mayank7cb@gmail.com>
2020-05-10 21:29:13 +0530

    support to install gcc and cmake using script

--- setup.sh
+++ setup.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+# install and check for gcc
+sudo apt-get -y install build-essential
+
+# install cmake
+sudo apt-get -y install cmake
+
 # check and install souffle
 if [ $(dpkg-query -W -f='${Status}' souffle 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
 	  echo "deb https://dl.bintray.com/souffle-lang/deb-unstable bionic main" | sudo tee -a /etc/apt/sources.list
 	  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
 	  sudo apt-get update
-	  sudo apt-get install souffle
+	  sudo apt-get -y install souffle
 fi
 
 # install python
 sudo apt-get install python3
 
 # get boost libraries
-sudo apt-get install libpthread-workqueue0 libboost-all-dev
+sudo apt-get -y install libpthread-workqueue0 libboost-all-dev
 
