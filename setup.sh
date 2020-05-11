d8af1a0b17f8fba882a73ca4141c2a583eb3ae19
Mayank <mayank7cb@gmail.com>
2020-05-12 01:33:39 +0530

    Updates for souffle ppa entry failure handling

--- setup.sh
+++ setup.sh
@@ -9,7 +9,13 @@ sudo apt-get -y install cmake
 # check and install souffle
 if [ $(dpkg-query -W -f='${Status}' souffle 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-	  echo "deb https://dl.bintray.com/souffle-lang/deb-unstable bionic main" | sudo tee -a /etc/apt/sources.list
+	  the_ppa="https://dl.bintray.com/souffle-lang/deb-unstable bionic main"
+
+	  if ! grep -q "^deb .*$the_ppa" /etc/apt/sources.list; 
+	  then
+		echo "deb https://dl.bintray.com/souffle-lang/deb-unstable bionic main" | sudo tee -a /etc/apt/sources.list
+	  fi
+
 	  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
 	  sudo apt-get update
 	  sudo apt-get -y install souffle
